59c76a042cc4bdbef54b62c933524074e24350db
ma74ni <ma74ny@gmail.com>
2024-05-16 00:20:26 -0500

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5Ho5mDAQ7r0GwAgL10Jy5lLVStEm1JtE",
+  authDomain: "survery-response.firebaseapp.com",
+  projectId: "survery-response",
+  storageBucket: "survery-response.appspot.com",
+  messagingSenderId: "929392254863",
+  appId: "1:929392254863:web:bd8b8340fc847d2eefb5fa"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+
+export { db };
